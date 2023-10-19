0f240d8dbcf8e4061c99df8ef1fc1659f465389a
weibin2537 <2537444533@qq.com>
2023-10-19 17:04:23 +0800

    ioc-dapp v1

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xfDE15eE4A5aB96A9Ed60965d053967767923225d";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY_URL = process.env.SEPOLIA_RPC_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity:{
+version:"0.8.21",
+compilers: [{ path: "/opt/homebrew/Cellar/solidity/0.8.21/" }]},
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
+
